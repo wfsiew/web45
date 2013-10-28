ee20e0b03d4e76db3635ec8edd8266082d368e44
wfsiew <siewwingfei@hotmail.com>
2013-10-29 00:40:51 +0800

    commit

---
+++ Web/Pages/Admin/Admin.Master.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Web.Routing
+
+Public Class Admin
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Public ReadOnly Property OrdersUrl() As String
+        Get
+            Return generateURL("admin_orders")
+        End Get
+    End Property
+    Public ReadOnly Property ProductsUrl() As String
+        Get
+            Return generateURL("admin_products")
+        End Get
+    End Property
+
+    Private Function generateURL(routeName As String) As String
+        Return RouteTable.Routes.GetVirtualPath(
+        Nothing,
+        routeName,
+        Nothing).VirtualPath
+    End Function
+End Class
